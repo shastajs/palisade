b834d384187a961c2abe460acf53f206ff14339a
Contra <contra@maricopa.edu>
2016-01-27 16:52:10 -0800

    build

--- dist/screenDeep.js
+++ dist/screenDeep.js
@@ -8,7 +8,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _lodash = require('lodash.map');
+var _lodash = require('lodash.reduce');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
@@ -18,26 +18,31 @@ var _lodash4 = _interopRequireDefault(_lodash3);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var screenDeep = function screenDeep(user, data) {
+var screenDeep = function screenDeep(user, data, returnEmpty) {
+  if ((typeof data === 'undefined' ? 'undefined' : (0, _typeof3.default)(data)) !== 'object') return data;
+
   // check if the user can even see the doc
-  if (data && data.authorized && !data.authorized(user, 'read')) {
-    return;
+  if (data.authorized && !data.authorized('read', user)) {
+    if (returnEmpty) return;
+    return Array.isArray(data) ? [] : {};
   }
   // single instance w/ lens
-  if (data && data.lens) {
-    return data.lens(user, 'read');
+  if (data.screen) {
+    return data.screen('read', user);
   }
 
   // array of instances w/ lens
   if (Array.isArray(data)) {
-    return (0, _lodash2.default)(data, screenDeep.bind(null, user));
+    return (0, _lodash2.default)(data, function (p, v) {
+      var nv = screenDeep(user, v, true);
+      if (nv != null) p.push(nv);
+      return p;
+    }, []);
   }
 
-  if ((typeof data === 'undefined' ? 'undefined' : (0, _typeof3.default)(data)) === 'object') {
-    return (0, _lodash4.default)(data, screenDeep.bind(null, user));
-  }
-
-  return data;
+  return (0, _lodash4.default)(data, function (v) {
+    return screenDeep(user, v, true);
+  });
 };
 
 exports.default = screenDeep;
